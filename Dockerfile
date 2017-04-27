16461da0675edc05bf6f8784bf6128b4d5485b1f
Hirofumi Tanigami <tanigami@gmail.com>
2017-04-28 02:01:25 +0900

    Styles

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,11 @@
 FROM php:7.1.1-fpm-alpine
 
+RUN apk update && \
+    apk add --no-cache git mysql-client curl libmcrypt libmcrypt-dev openssh-client \
+    libxml2-dev freetype-dev libpng-dev libjpeg-turbo-dev g++ make autoconf nodejs python && \
+    rm -rf /tmp/src && \
+    rm -rf /var/cache/apk/*
+
 RUN docker-php-ext-install \
     pdo_mysql \
     mysqli \
@@ -7,6 +13,9 @@ RUN docker-php-ext-install \
 
 RUN curl -sS https://getcomposer.org/installer | \
     php -- --install-dir=/usr/bin/ --filename=composer
+
+RUN npm i -g gulp && \
+    rm -rf /tmp/*
 
 ENV LARAVEL_ROOT /laravel
 RUN mkdir -p $LARAVEL_ROOT
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,163 +10,7 @@
     <link rel="stylesheet" href="/css/normalize.css">
     <link rel="stylesheet" href="https://cloud.typography.com/6830252/7572172/css/fonts.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Ubuntu+Mono">
-    <style>
-        html {
-            height: 100%;
-        }
-        body,
-        header {
-            height: 100%;
-        }
-
-        body {
-            font-family: "Didot 16 A", "Didot 16 B", "Ryumin Regular KL", serif;
-            font-style: normal;
-            font-weight: 300;
-            background: #000;
-            color: #fff;
-            text-align: center;
-        }
-
-        header {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-        }
-
-
-        h1 {
-
-            font-size: 1.5vw;
-            letter-spacing: 0.05vw;
-            margin: 0 0 2vw;
-        }
-
-        h1 > span {
-            display: block;
-
-            font-size: 8vw;
-
-            font-family: "Didot 64 A", "Didot 64 B";
-            font-style: italic;
-            font-weight: 300;
-            letter-spacing: 0.7vw;
-        }
-
-        blockquote {
-            left: 0;
-            margin: 0;
-            position: fixed;
-            text-align: left;
-            top: 0;
-        }
-        blockquote > pre {
-            margin: 0;
-            font-family: 'Ubuntu Mono', monospace;
-        }
-        blockquote > pre:last-child {
-            color: #00c120;
-        }
-
-        .container {
-            margin: 0 auto;
-            width: 50vw;
-        }
-
-        form {
-            margin: -16vw 0 15vw;
-        }
-
-        textarea {
-            display: block;
-            background: #222;
-            color: #fff;
-            border: none;
-            box-sizing: border-box;
-            text-align: center;
-            width: 100%;
-
-            font-family: "Didot 24 A", "Didot 24 B", "Ryumin Regular KL", serif;
-            font-style: normal;
-            font-weight: 300;
-            font-size: 2.5vw;
-            letter-spacing: .05em;
-
-            line-height: 1.5;
-
-            padding: 1vw;
-
-            outline: 0;
-        }
-
-        button {
-            font-family: "Didot 24 A", "Didot 24 B", "Ryumin Regular KL", serif;
-            font-style: normal;
-            font-weight: 300;
-            display: block;
-            color: #fff;
-            width: 100%;
-            border: none;
-            border-radius: 0;
-            background: #333;
-            letter-spacing: .05em;
-            -webkit-appearance: none;
-            outline: 0;
-            transition-duration: .5s;
-            font-size: 2vw;
-            padding: 1vw;
-        }
-        button:hover {
-            background: #444;
-        }
-
-        section {
-            margin-bottom: 15vw;
-        }
-
-        section p {
-            font-family: "Didot 24 A", "Didot 24 B", "Ryumin Regular KL", serif;
-            font-style: normal;
-            font-weight: 300;
-            font-size: 2.5vw;
-            letter-spacing: .05em;
-            line-height: 1.5;
-            margin: 0 0 2vw;
-        }
-
-        section div {
-            font-family: "Didot 16 A", "Didot 16 B";
-            font-style: normal;
-            font-weight: 300;
-            font-size: 1.5vw;
-            letter-spacing: .05em;
-        }
-        section div a {
-            color: #666;
-            text-decoration: none;
-            border-bottom: 1px solid #555;
-        }
-
-        section div {
-            font-family: "Didot 16 A", "Didot 16 B";
-            font-style: normal;
-            font-weight: 300;
-            font-size: 1.5vw;
-            letter-spacing: .05em;
-        }
-
-        .connect {
-            margin-top: -40vh;
-            display: block;
-            background: #333;
-            color: #fff;
-            letter-spacing: .05em;
-            transition-duration: .5s;
-            font-size: 2vw;
-            padding: 1vw 4vw;
-            text-decoration: none;
-        }
-    </style>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     <script src="//webfont.fontplus.jp/accessor/script/fontplus.js?JItqYG3JnkE%3D&box=PkIVeFGe~ik%3D&aa=1"></script>
 </head>
 <body>
@@ -182,28 +26,36 @@ Updating dependencies (including require-dev)
       </pre>
 </blockquote>
 
-<header>
-    <div>
-        <h1>What are you doing while waiting for <span>Composer ?</span></h1>
-    </div>
-</header>
+<div class="filler">
+    <header>
+        <div>
+            <h1>What are you doing while waiting for <span>COMPOSER ?</span></h1>
+        </div>
+    </header>
+</div>
 
 <div class="container">
     @if (!Auth::check())
-        <a class="connect" href="/connect">Let the world know</a>
+        <div class="filler">
+            <a class="connect" href="/connect">Let the world know</a>
+        </div>
     @else
-        <form method="post" action="/post/store">
-            {{ csrf_field() }}
-            <textarea name="body" placeholder="You wouldn’t say nothing…" rows="3" required maxlength="100"></textarea>
-            <button>Let the world know</button>
-        </form>
+        <div class="filler">
+            <form method="post" action="/post/store">
+                {{ csrf_field() }}
+                <textarea name="body" placeholder="You wouldn’t say nothing…" rows="3" required maxlength="100"></textarea>
+                <button>Let the world know</button>
+            </form>
+        </div>
     @endif
 
     @foreach ($posts as $post)
-        <section>
-            <p>{{ $post->body }}</p>
-            <div><a href="https://github.com/{{ $post->user->github_username }}">{{ $post->user->github_username }}</a></div>
-        </section>
+        <div class="filler">
+            <section class="post">
+                <p class="post__body">{{ $post->body }}</p>
+                <a class="post__github-link" href="https://github.com/{{ $post->user->github_username }}">{{ $post->user->github_username }}</a>
+            </section>
+        </div>
     @endforeach
 </div>
 <script>
